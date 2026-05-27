1fbad4f8a93b7810e5befb2506a874f1956ef793
Shirley Butti <shirleybutti12@gmail.com>
2026-05-27 12:25:20 +0400

    get ,delete and update user api

--- src/App.js
+++ src/App.js
@@ -14,19 +14,49 @@ connectDB()
     console.log(err);
   });
 
+app.use(express.json()); //json niddleware
+
+//dybamic signup
 app.post("/singUp", async (req, res) => {
-  const user = new User({
-    firstName: "Jills",
-    lastName: "Butti",
-    emailId: "Jills@gmail.com",
-    password: "abasssc@11",
-    age: 12,
-    gender: "Female",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("Data Added Successfully.");
   } catch (err) {
-    res.status(400).send("Data not added" + err)
+    res.status(400).send("Data not added" + err);
+  }
+});
+
+//get User data
+app.get("/getUser", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    console.log(user);
+    if (!user) {
+      res.status(502).send("Data not found");
+    } else {
+      res.send("Data fetched Successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+//delete record
+app.delete("/delete", async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ lastName: req.body.lastName });
+    res.send("Data deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.patch("/update", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body.userId ,{firstName :"Jillu"});
+    res.send("Data Updated Successfully");
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
   }
 });
