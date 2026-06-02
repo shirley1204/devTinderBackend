50b92597f4cfbfc622e21dcfe15b0e5dee17b16b
Shirley Butti <shirleybutti12@gmail.com>
2026-06-02 21:27:58 +0400

    feed Api with pagination using skip() and limit()

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { UserAuth } = require("../utils/validations");
 const ConnectionRequest = require("../models/Connection");
+const User = require("../models/User");
 
 const Router = express.Router();
 
@@ -24,13 +25,53 @@ Router.get("/user/connections", UserAuth, async (req, res) => {
   try {
     const LoggedInuser = req.user._id;
     const getConnections = await ConnectionRequest.find({
-     $or: [{ toUserId: LoggedInuser ,status :"Accepted"},
-      { fromUserId: LoggedInuser,status:"Accepted"}]}
-    ).populate("fromUserId", "firstName lastName").populate("toUserId", "firstName lastName");
+      $or: [
+        { toUserId: LoggedInuser, status: "Accepted" },
+        { fromUserId: LoggedInuser, status: "Accepted" },
+      ],
+    })
+      .populate("fromUserId", "firstName lastName")
+      .populate("toUserId", "firstName lastName");
 
     res.json({
       message: "data fetched successfully",
       data: getConnections,
+    });
+  } catch (err) {
+    res.status(400).send("Something Went Wrong" + err);
+  }
+});
+
+Router.get("/feed", UserAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) ||10;
+    const skip = (page -1) * limit;
+    const LoggedInuser = req.user._id;
+    const getConnections = await ConnectionRequest.find({
+      $or: [{ toUserId: LoggedInuser }, { fromUserId: LoggedInuser }],
+    })
+      .select("fromUserId toUserId")
+      .populate("fromUserId", "firstName");
+
+    const blockedUser = new Set();
+    getConnections.forEach((item) => {
+      blockedUser.add(item.toUserId);
+      blockedUser.add(item.fromUserId);
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(blockedUser) } },
+        {
+          _id: { $ne: LoggedInuser },
+        },
+      ],
+    }).select("firstName lastName").skip(skip).limit(limit);
+
+    res.json({
+      message: "data fetched successfully",
+      data: users,
     });
   } catch (err) {
     res.status(400).send("Something Went Wrong" + err);
