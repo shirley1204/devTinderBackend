7cdc346a55107dfec9784106d5de0e40b12a23c2
Shirley Butti <shirleybutti12@gmail.com>
2026-06-08 13:25:20 +0400

    Connection api changes

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -23,20 +23,25 @@ Router.get("/user/request/recieved", UserAuth, async (req, res) => {
 
 Router.get("/user/connections", UserAuth, async (req, res) => {
   try {
-    const LoggedInuser = req.user._id;
+    const LoggedInuser = req?.user._id;
     const getConnections = await ConnectionRequest.find({
       $or: [
         { toUserId: LoggedInuser, status: "Accepted" },
         { fromUserId: LoggedInuser, status: "Accepted" },
       ],
     })
-      .populate("fromUserId", "firstName lastName")
-      .populate("toUserId", "firstName lastName");
-
-    res.json({
-      message: "data fetched successfully",
-      data: getConnections,
+      .populate("fromUserId", "firstName lastName age gender about _id")
+      .populate("toUserId", "firstName lastName age gender about _id");
+    
+    
+     const data = getConnections.map((row) => {
+      if (row?.fromUserId._id.toString() === LoggedInuser.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
     });
+
+    res.json({ data });
   } catch (err) {
     res.status(400).send("Something Went Wrong" + err);
   }
@@ -59,6 +64,8 @@ Router.get("/feed", UserAuth, async (req, res) => {
       blockedUser.add(item.toUserId);
       blockedUser.add(item.fromUserId);
     });
+
+    console.log("blockedUser",blockedUser)
 
     const users = await User.find({
       $and: [
